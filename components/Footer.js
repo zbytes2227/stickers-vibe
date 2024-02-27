fbfe581e4d7d3ad955510576655dbbbf0cc35998
Zbytes <111581344+zbytes2227@users.noreply.github.com>
2024-02-27 21:26:00 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -87,7 +87,7 @@ const Footer = () => {
                 </div>
                 <div className="bg-gray-100">
                     <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-                        <p className="text-gray-500 text-sm text-center sm:text-left">© 2023 Stickers Vibe —
+                        <p className="text-gray-500 text-sm text-center sm:text-left">© 2024 Stickers Vibe —
                             <Link href={''} rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">Tejas Awasthi</Link>
                         </p>
                         <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
