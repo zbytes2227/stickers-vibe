a107c8f94345dc7b3f2d22bbfd5311644778510b
Ujjwal Kushwaha <111581344+zbytes2227@users.noreply.github.com>
2024-10-12 20:50:27 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -88,7 +88,7 @@ const Footer = () => {
                 <div className="bg-gray-100">
                     <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
                         <p className="text-gray-500 text-sm text-center sm:text-left">© 2024 Stickers Vibe —
-                            <Link href={''} rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">Tejas Awasthi</Link>
+                            <Link href={'https://www.linkedin.com/in/shikhar-srivastava-72aa3a332/'} rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">Shikhar Srivastava</Link>
                         </p>
                         <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
                             <a className="text-gray-500">
